88783bb1fddaa24e579d972259ab3b432613f188
Lanre Adedara <lanre_samuel2002@yahoo.com>
2020-03-11 15:26:19 +0100

    added ffm logo

--- components/layout/header-menu.vue
+++ components/layout/header-menu.vue
@@ -12,51 +12,6 @@
         </nuxt-link>
       </div>
       <!--End: Logo-->
-
-      <!--Navigation Resposnive Trigger-->
-      <div id="mainMenu-trigger">
-        <button class="lines-button x">
-          <span class="lines"></span>
-        </button>
-      </div>
-      <!--end: Navigation Resposnive Trigger-->
-
-      <!--Navigation-->
-      <!-- <div id="mainMenu" class="light">
-        <div class="container">
-          <nav>
-            <ul>
-              <li>
-                <nuxt-link to="/">Home</nuxt-link>
-              </li>
-              <li class="dropdown" :class="{active: (currentActiveRoutes.includes('what-we-do'))}">
-                <a href="#">What We Do</a>
-                <ul class="dropdown-menu">
-                  <li>
-                    <nuxt-link to="/what-we-do/services">Our Services</nuxt-link>
-                  </li>
-                  <li>
-                    <nuxt-link to="/what-we-do">PropLab.Africa</nuxt-link>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <nuxt-link to="/who-we-are">Who We Are</nuxt-link>
-              </li>
-              <li>
-                <nuxt-link to="/news">News</nuxt-link>
-              </li>
-              <li>
-                <nuxt-link to="/careers">Careers</nuxt-link>
-              </li>
-              <li>
-                <nuxt-link to="/contact-us">Contact Us</nuxt-link>
-              </li>
-            </ul>
-          </nav>
-        </div>
-      </div> -->
-      <!--end: Navigation-->
     </div>
   </div>
 </template>
